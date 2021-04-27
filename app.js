142dfb754bec6663ba07adb41f471b9bff8283ca
Candelario <ccampanaon@users.noreply.github.com>
2021-04-26 20:13:10 -0600

    added entri point

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const users = require('./src/apiServices/users/routes');
+
+require('dotenv').config();
+
+const app = express();
+const env = path.join(__dirname, '.env');
+require('dotenv').config({path: path.join(__dirname, '.env')});
+const port = process.env.port || 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json()); 
+const db = require('./src/config/connection');
+
+const Users = db.users;
+// force: true will drop the table if it already exists
+db.sequelize.sync({force: false , alter : true}).then(() => {
+    console.log('Drop and Resync with { force: true }');
+  });
+app.get('/', (req, res) => {
+  res.send('Welcome to NODE JS AWS...')
+});
+users.usersRoutes(app);
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  });
